9c2cad49703fc0b84673797c25d9dcebadc0f936
Minsu_Kang <powerful@postech.ac.kr>
2018-12-10 17:43:14 +0900

    Piece 클래스의 내부 메소드 구현

--- block.cpp
+++ block.cpp
@@ -1,6 +1,116 @@
 #include "block.h"
 
-Block::Block()
-{
 
+Piece::Piece(int shape, int color)
+	:_shape(shape)
+	,_color(color)
+	,_pos(middle_x , -1)
+{
+	switch (_shape)
+	{
+	case Piece::SHAPE_I:
+		_blocks[0] = Coordinate(0,-2);
+		_blocks[1] = Coordinate(0,-1);
+		_blocks[2] = Coordinate(0,0);
+		_blocks[3] = Coordinate(0,1);
+		break;
+	case Piece::SHAPE_O:
+		_blocks[0] = Coordinate(0, 0);
+		_blocks[1] = Coordinate(1, 0);
+		_blocks[2] = Coordinate(0, 1);
+		_blocks[3] = Coordinate(1, 1);
+		break;
+	case Piece::SHAPE_Z:
+		_blocks[0] = Coordinate(-1, 0);
+		_blocks[1] = Coordinate(0, 0);
+		_blocks[2] = Coordinate(0, 1);
+		_blocks[3] = Coordinate(1, 1);
+		break;
+	case Piece::SHAPE_S:
+		_blocks[0] = Coordinate(-1, 1);
+		_blocks[1] = Coordinate(0, 1);
+		_blocks[2] = Coordinate(0, 0);
+		_blocks[3] = Coordinate(1, 1);
+		break;
+	case Piece::SHAPE_J:
+		_blocks[0] = Coordinate(0, -1);
+		_blocks[1] = Coordinate(0, 0);
+		_blocks[2] = Coordinate(0, 1);
+		_blocks[3] = Coordinate(1, 1);
+		break;
+	case Piece::SHAPE_L:
+		_blocks[0] = Coordinate(0, -1);
+		_blocks[1] = Coordinate(0, 0);
+		_blocks[2] = Coordinate(0, 1);
+		_blocks[3] = Coordinate(-1, 1);
+		break;
+	case Piece::SHAPE_T:
+		_blocks[0] = Coordinate(-1, 0);
+		_blocks[1] = Coordinate(0, 0);
+		_blocks[2] = Coordinate(1, 0);
+		_blocks[3] = Coordinate(0, 1);
+		break;
+	}
+}
+
+void Piece::rotate(int direction)
+{
+	int temp;
+	switch (direction)
+	{
+	case CW:
+		for (int i = 0; i < 4; i++)
+		{
+			temp = _blocks[i].x;
+			_blocks[i].x = _blocks[i].y;
+			_blocks[i].y = -temp;
+		}
+		break;
+	case CCW:
+		for (int i = 0; i < 4; i++)
+		{
+			temp = _blocks[i].x;
+			_blocks[i].x = -_blocks[i].y;
+			_blocks[i].y = temp;
+		}
+		break;
+	}
+}
+
+void Piece::left()
+{
+	_pos.x--;
+}
+
+void Piece::right()
+{
+	_pos.x++;
+}
+
+void Piece::down()
+{
+	_pos.y++;
+}
+
+bool Piece::isOverlapped(Coordinate block_pos)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if ((_pos + _blocks[i]) == block_pos)
+			return true;
+	}
+	return false;
+}
+
+QList<Block> Piece::blocks()
+{
+	QList<Block> b_list;
+	for (int i = 0; i < 4; i++)
+	{
+		Block b;
+		b.color = _color;
+		b.pos = _pos + _blocks[i];
+		b_list << b;
+	}
+	return b_list;
 }
--- block.h
+++ block.h
@@ -1,12 +1,27 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include<QList>
+#include <QList>
+#include <QDebug>
 
 struct Coordinate
 {
     int x;
     int y;
+
+	Coordinate()
+		:x(-1), y(-1) {}
+	Coordinate(int x, int y)
+		:x(x), y(y) {}
+	Coordinate operator+(const Coordinate &c) const{
+		Coordinate result;
+		result.x = x + c.x;
+		result.y = y + c.y;
+		return result;
+	}
+	bool operator==(const Coordinate &c) const {
+		return (x == c.x) && (y == c.y);
+	}
 };
 
 struct Block{
@@ -17,16 +32,22 @@ struct Block{
 class Piece
 {
 public:
-    Piece(int shape, int color,int type);
-    void rotate();
-    void move();
+	enum {CW, CCW};
+	enum SHAPE {SHAPE_I, SHAPE_O, SHAPE_Z, SHAPE_S, SHAPE_J, SHAPE_L, SHAPE_T };
+
+    Piece(int shape, int color);
+    void rotate(int direction);
+	void left();
+	void right();
+	void down();
     bool isOverlapped(Coordinate block_pos);
     QList<Block> blocks();
 private:
+	static const int middle_x = 10;
     Coordinate _pos;
     Coordinate _blocks[4];
-    int shape;
-    int color;
+    int _color;
+	int _shape;
 };
 
 #endif // BLOCK_H
