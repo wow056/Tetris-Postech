78686f7e9b28562c1820fcafaa044c1463f82e36
Minsu_Kang <powerful@postech.ac.kr>
2018-12-10 16:16:31 +0900

    함수 진입접과 탈출점에 qDebug 메시지 출력

--- game.cpp
+++ game.cpp
@@ -6,18 +6,21 @@ Game::Game(QObject *parent = nullptr)
 	:QObject(parent),
 	nextPiece(0, 0), currentPiece(0, 0)
 {
+	qDebug() << "entering:	Game::Game";
 	timer = new QTimer(this);
 	timer_interbal = 1000;
 
 	//timer_interbal 시간이 지날 때마다 update 함수가 실행됨.
 	connect(timer, SIGNAL(timeout()), this, SLOT(update()));
 	timer->start(timer_interbal);
+	qDebug() << "exiting:	Game::Game";
 }
 
 
 
 void Game::setInput(int input)
 {
+	qDebug() << "entering:	Game::setInput";
 	switch (input)
 	{
 	case Qt::Key_Left:
@@ -37,12 +40,76 @@ void Game::setInput(int input)
 			setBlockRotate(RIGHT);
 		break;
 	}
+	qDebug() << "entering:	Game::setInput";
 }
 
 void Game::update()
 {
+	qDebug() << "entering:	Game::update";
 	if (isBlockDrop())
 		setBlockDrop();
 	if (isLineComplete())
 		deleteLine();
+	qDebug() << "exiting:	Game::update";
+}
+
+bool Game::isGameover() const
+{
+	return false;
+}
+
+bool Game::isBlockDrop() const
+{
+	return false;
+}
+
+void Game::deleteLine()
+{
+	qDebug() << "entering:	Game::deleteLine";
+	putOutput();
+	qDebug() << "exiting:	Game::deleteLine";
+}
+
+void Game::setBlockDrop()
+{
+	qDebug() << "entering:	Game::setBlockDrop";
+	putOutput();
+	qDebug() << "exiting:	Game::setBlockDrop";
+}
+
+void Game::setBlockMove(int direction)
+{
+	qDebug() << "entering:	Game::setBlockMove";
+	putOutput();
+	qDebug() << "exiting:	Game::setBlockDrop";
+}
+
+void Game::setBlockRotate(int direction)
+{
+	qDebug() << "entering:	Game::setBlockRotate";
+	putOutput();
+	qDebug() << "exiting:	Game::setBlockRotate";
+}
+
+void Game::saveBlock()
+{
+	qDebug() << "entering:	Game::saveBlock";
+	putOutput();
+	qDebug() << "exiting:	Game::saveBlock";
+}
+
+void Game::setNextPiece()
+{
+	qDebug() << "entering:	Game::setNextPiece";
+	emit sendNextBlock(nextPiece);
+	qDebug() << "exiting:	Game::setNextPiece";
+}
+
+void Game::putOutput()
+{
+	QList<Block> output;
+	qDebug() << "entering:	Game::putOutput";
+	output = savedBlocks + currentPiece.blocks();
+	emit updateBoard(output);
+	qDebug() << "exiting:	Game::putOutput";
 }
